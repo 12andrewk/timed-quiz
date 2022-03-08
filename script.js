79a6f0f0cb247ad7bea1f3420e3f92dff84e2095
Andrew Kim <sendandrewkim@gmail.com>
2022-03-07 21:32:40 -0800

    finished timer and end screen

--- script.js
+++ script.js
@@ -60,32 +60,51 @@ var optionSelected = "";
 // Total points placeholder
 var pointsIndex = 0;
 
-var timer = document.getElementById("timer")
+
+// Timer elements
+var timer = document.getElementById("timer");
 var timeIndex = 60;
 
-
-function timerStart() {
-
-
-    clearTimeout
+function timerCountDown() {
+    if (timeIndex > 0) {
+        timeIndex--;
+        timer.innerHTML = "Time: " + timeIndex;
+    } else {
+        quizEnd();
+        timer.setAttribute("style", "display:none");
+        clearInterval(timeLeft);
+    }
 }
 
+// Starting timer
+function timerStart() {
+    timeLeft = setInterval(timerCountDown, 1000);
+    timer.setAttribute("style", "display:visible");
+}
+
+// setInterval()
+// setTimeout()
+// clearInterval()
+// clearTimeout()
 
 // Starts quiz, executed by start button
 function quizStart() {
     // Hides sub-heading text and button on front page
     contentFront.style.display = "none";
 
-
-    // Displays question on front page
-    questionDisplay.setAttribute("style", "display:visible");
     // Displays options on front page
     optionDisplay0.setAttribute("style", "display:visible");
     optionDisplay1.setAttribute("style", "display:visible");
     optionDisplay2.setAttribute("style", "display:visible");
     optionDisplay3.setAttribute("style", "display:visible");
+
+    // Displays question on front page
+    questionDisplay.setAttribute("style", "display:visible");
     // Fetches the questions that are to be displayed
     questionFetch(questionIndex);
+
+    // Starts timer
+    timerStart();
 }
 
 
@@ -105,19 +124,19 @@ function questionFetch() {
 
     // Event listener for selecting an options
     optionDisplay0.addEventListener("click", () => {
-        optionSelected = questionList[questionIndex].questionOption[0].value;
+        optionSelected = questionList[questionIndex].questionOption[0];
         optionCheck();
     });
     optionDisplay1.addEventListener("click", () => {
-        optionSelected = questionList[questionIndex].questionOption[1].value;
+        optionSelected = questionList[questionIndex].questionOption[1];
         optionCheck();
     });
     optionDisplay2.addEventListener("click", () => {
-        optionSelected = questionList[questionIndex].questionOption[2].value;
+        optionSelected = questionList[questionIndex].questionOption[2];
         optionCheck();
     });
     optionDisplay3.addEventListener("click", () => {
-        optionSelected = questionList[questionIndex].questionOption[3].value;
+        optionSelected = questionList[questionIndex].questionOption[3];
         optionCheck();
     });
 }
@@ -125,34 +144,37 @@ function questionFetch() {
 
 function optionCheck() {
     // If the correct option is selected
-    if (optionSelected == questionList.questionAnswer) {
+    if (questionIndex > 3) {
+        quizEnd();
+    } else if (optionSelected == questionList[questionIndex].questionAnswer) {
         // Proceeds to next question set
         questionIndex += 1;
         // Points added
         pointsIndex += 20;
         // Show next question with new questionIndex
         questionDisplay.textContent = questionList[questionIndex].questionProblem;
-    } else if (questionIndex == 5) {
-        // After the 5th question quiz ends
-        quizEnd();
+        // Show next question
+        optionDisplay0.textContent = questionList[questionIndex].questionOption[0];
+        optionDisplay1.textContent = questionList[questionIndex].questionOption[1];
+        optionDisplay2.textContent = questionList[questionIndex].questionOption[2];
+        optionDisplay3.textContent = questionList[questionIndex].questionOption[3];
     } else {
-
-        questionIndex += 1;
         // Time penalty for incorrect answer
-        // timeIndex -= 5;
+        timeIndex -= 5;
     }
 }
-
 
 // Quiz ending
 function quizEnd() {
     // Hides quiz
-    optionDisplay0.setAttribute("style", "display:none");
-    optionDisplay1.setAttribute("style", "display:none");
-    optionDisplay2.setAttribute("style", "display:none");
-    optionDisplay3.setAttribute("style", "display:none");
-    questionDisplay.setAttribute("style", "display:none");
+    optionDisplay0.style.display = "none";
+    optionDisplay1.style.display = "none";
+    optionDisplay2.style.display = "none";
+    optionDisplay3.style.display = "none";
+    questionDisplay.style.display = "none";
 
+    clearInterval(timeLeft);
+    timer.setAttribute("style", "display:none");
     end.setAttribute("style", "display:visible");
 }
 
